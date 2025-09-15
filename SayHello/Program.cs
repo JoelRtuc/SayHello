4b890abd4fdd79ef33e31f686d4e7473819f261c
Joel Ringh <joel.ringh@gmail.com>
2025-09-15 09:56:59 +0200

    Why

---
+++ SayHello/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        SayHello(Console.ReadLine());
+
+        void SayHello(string name)
+        {
+            Console.WriteLine("Hej " + name + " välkommen till programmet!");
+        }
+    }
+}
